0a70be2835cf42ae533370d6ff2d8490b0e7f8c3
lincyrajasekaran3 <hp@DESKTOP-6433J6U>
2025-01-27 17:48:35 +0530

    created user model and controller

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express= require("express");
+const app = express()
+
+if(process.env.NODE_ENV !=="PRODUCTION"){
+    require("dotenv").config({
+        path:"backend/config/.env"
+    });
+};
+
+// app.use(ErrorHandler);
+module.exports =app;
