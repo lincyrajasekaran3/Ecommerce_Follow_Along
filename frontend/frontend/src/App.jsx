4e3ca5844bae80137da98616ebd5081187113eaf
lincyrajasekaran3 <hp@DESKTOP-6433J6U>
2025-02-06 15:05:55 +0530

    home page created

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -2,14 +2,14 @@
 
 import './App.css';
 import React from 'react';
-import {Login, Signup} from "./Routes/Routes"
+import {Login, Signup, Home} from "./Routes/Routes"
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-    <Route path='/' element={<h1>home</h1>}/>
+    <Route path='/' element={<Home/>}/>
     <Route path='/login' element={<Login/>}/>
     <Route path='/signup' element={<Signup/>}/>
     </Routes>
--- frontend/frontend/src/Routes/Routes.jsx
+++ frontend/frontend/src/Routes/Routes.jsx
@@ -1,3 +1,4 @@
 import Login from "../pages/login.jsx";
 import Signup from "../pages/signup.jsx";
-export { Login,Signup}
+import Home from "../pages/home.jsx"; 
+export { Login,Signup,Home}
